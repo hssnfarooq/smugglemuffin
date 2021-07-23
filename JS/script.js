1e9554cb61dcc225f4aa13d8a4e8e6d1dc0ed961
hssnfarooq <hhhassanhafeez91@gmail.com>
2021-07-23 17:18:18 +0500

    update

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+var up = document.getElementById('up');
+var down = document.getElementById('down');
+var right = document.getElementById('right');
+var left = document.getElementById('left');
+var keys = document.querySelector('.rectangle-btn');
+var messages = document.querySelector('.rectangle-messages');
+
+
+var combination = [3, 0, 2, 3, 0, 1, 2, 3, 1];
+var arrayWithElements = new Array();
+
+function clickListener(e) {
+  var clickedElement = window.event ? window.event.srcElement : e.target,
+    tags = document.getElementsByTagName(clickedElement.tagName);
+
+  for (var i = 0; i < tags.length; ++i) {
+    if (tags[i] == clickedElement) {
+      arrayWithElements.push(i);
+      if (JSON.stringify(combination) == JSON.stringify(arrayWithElements)) {
+        keys.style.display = 'none';
+        messages.style.display = 'block';
+      }
+    }
+  }
+}
